91d66d098799c93bb7ce9975b3a5c7336391b0b9
Yusufn22 <Yusufnaderi24@gmail.com>
2024-05-06 10:36:11 -0400

    labelmaker

---
+++ ObjectScripts/label_maker.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let myInfo = {
+    name: "Yusuf Naderi",
+    address: "43-44 kissena blvd",
+    city: "Queens",
+    state: "New York",
+    zip:"11355",
+}
+console.log(myInfo)
+
+
+
